8cd662f3a4b998000cd3fcb00a29fcf0507982f4
ZJVieth <52859000+ZJVieth@users.noreply.github.com>
2021-06-21 16:33:02 +0200

    Updates

---
+++ precache-manifest.980a4d13e1dccd0a402f30d2a8e75b96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0ce6bdd07f1eedc31579afe17faceb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5579ae2e09998d326219",
+    "url": "./static/css/main.84498f65.chunk.css"
+  },
+  {
+    "revision": "d045804389161e931aed",
+    "url": "./static/js/2.3a0af9ff.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.3a0af9ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5579ae2e09998d326219",
+    "url": "./static/js/main.1908d217.chunk.js"
+  },
+  {
+    "revision": "3d32699b116aa02612d9",
+    "url": "./static/js/runtime-main.5687438e.js"
+  }
+]);
